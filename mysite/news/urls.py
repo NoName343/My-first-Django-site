4c93b26d473eb2bf5ce1c0b85cd15f3b2d718b39
Pasha <admin@supper.com>
2021-04-26 10:23:48 +0300

    release v1

---
+++ mysite/news/urls.py
@@ -0,0 +1,23 @@
+import debug_toolbar
+from django.urls import path, include
+from django.views.decorators.cache import cache_page
+
+
+from news.views import *
+
+urlpatterns = [
+    path('login/', user_login, name='login'),
+    path('logout/', user_logout, name='logout'),
+    path('register/', register, name='register'),
+    path('contact/', contact, name='contact'),
+    #path('', index, name='home'),
+    path('', cache_page(90) (HomeNews.as_view()), name='home'),
+    #path('category/<int:category_id>/', det_category, name='category'),
+    path('category/<int:category_id>/', NewsByCategory.as_view(), name='category'),
+    #path('news/<int:news_id>/', view_news, name='view_news'),
+    path('news/<int:pk>/', ViewNews.as_view(), name='view_news'),
+    #path('news/add-news', add_news, name='add_news'),
+    path('news/add-news', CreateNews.as_view(), name='add_news'),
+
+    path('__debug__/', include(debug_toolbar.urls)),
+]
